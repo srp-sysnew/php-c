3f0b1bf6bf71b5beea30c21e9b159db1eea047e4
yoshimura <yoshimura@srp.co.jp>
2015-02-12 17:42:58 +0900

    HelloWorld

---
+++ src/C/HelloWorld.php
@@ -0,0 +1,13 @@
+namespace C;
+
+class HelloWorld {
+
+    /**
+     * echo 'hello,world!! by srp-sysnew'
+     */
+    public static function echo()
+    {
+        echo 'hello,world!! by srp-sysnew';
+    }
+
+}
