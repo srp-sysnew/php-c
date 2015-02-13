115ab0aeb0ff3c15addc0d3a4c79d61c4d22a441
srp-sysnew <srp-sysnew@srp-tech.com>
2015-02-13 14:33:04 +0900

    Update Smarty.php

--- src/Smarty.php
+++ src/Smarty.php
@@ -7,4 +7,17 @@
 namespace C;
 
 class Smarty extends \Smarty {
+  /**
+   * 
+   */
+  function __construct()
+  {
+    parent::__construct();
+    
+    $this->left_delimiter = '{{';
+    $this->right_delimiter = '}}';
+    
+    $this->default_modifiers = array('default:""');
+    
+  }
 }
